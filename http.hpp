8e002a3b23539b5dd25cb147e53341c10dc955ef
JeonYoungHo-youjeon <dudgh11202@gmail.com>
2022-10-13 21:29:41 +0900

    [refactor] std::string 에서 string 으로 변경

--- http.hpp
+++ http.hpp
@@ -3,6 +3,8 @@
 
 # include <string>
 
+using std::string;
+
 typedef struct Request
 {
 	/*
@@ -16,15 +18,16 @@ typedef struct Request
 		all newline characters
 		replaced by crlf
 	*/
-	std::string method;			// GET, POST, DELETE
-	std::string URL;			// 요청 URL (/, /www...) 
-	std::string protocol;		// HTTP 버전 (HTTP/1.1)
-	std::string connection;		// 연결 옵션 (Close 만 구현)
-	std::string encoding;		// Transper-Encoding (chunked 만 구현)
-	std::string host;			// 서버의 호스트 명과 포트 (localhost:80)
-	std::string contentLength;	// 리퀘스트 메세지가 담고있는 화물(entity 엔터티 = body)의 길이
+
+	string method;			// GET, POST, DELETE
+	string URL;				// 요청 URL (/, /www...) 
+	string protocol;		// HTTP 버전 (HTTP/1.1)
+	string connection;		// 연결 옵션 (Close 만 구현)
+	string encoding;		// Transper-Encoding (chunked 만 구현)
+	string host;			// 서버의 호스트 명과 포트 (localhost:80)
+	string contentLength;	// 리퀘스트 메세지가 담고있는 화물(entity 엔터티 = body)의 길이
 	
-	std::string body;
+	string body;
 } Request;
 
 typedef struct Reponse 
@@ -45,18 +48,19 @@ typedef struct Reponse
 		<html>
 		</html>
 	*/
-	std::string protocol;		// HTTP 버전 (HTTP/1.1)
-	std::string statusCode;		// 상태 코드 (200, 404)
-	std::string reasonPhrase;	// 사유 구절 (OK, NOT FOUND)
-	std::string date;			// 메세지가 만들어진 시간과 날짜
-	std::string contentLength;	// 리퀘스트 메세지가 담고있는 엔터티 = body의 길이
-	std::string contentType;	// 리퀘스트 메세지가 담고있는 엔터티 = body의 타입 (text/html)
-	std::string encoding;		// Transper-Encoding (chunked 만 구현)
-	std::string connection;		// 연결 옵션 (Close 만 구현)
-	std::string server;			// 서버 애플리케이션의 이름과 버전
-	std::string location;		// 301 등에서 리소스의 위치를 알려줄때 사용
 
-	std::string body;
+	string protocol;		// HTTP 버전 (HTTP/1.1)
+	string statusCode;		// 상태 코드 (200, 404)
+	string reasonPhrase;	// 사유 구절 (OK, NOT FOUND)
+	string date;			// 메세지가 만들어진 시간과 날짜
+	string contentLength;	// 리퀘스트 메세지가 담고있는 엔터티 = body의 길이
+	string contentType;		// 리퀘스트 메세지가 담고있는 엔터티 = body의 타입 (text/html)
+	string encoding;		// Transper-Encoding (chunked 만 구현)
+	string connection;		// 연결 옵션 (Close 만 구현)
+	string server;			// 서버 애플리케이션의 이름과 버전
+	string location;		// 301 등에서 리소스의 위치를 알려줄때 사용
+
+	string body;
 } Reponse;
 
 #endif

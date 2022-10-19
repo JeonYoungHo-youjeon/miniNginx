a54c56b1123bbaedc8b42bc0015147137a20b7e2
youngpar <youngseo321@gmail.com>
2022-10-20 05:28:07 +0900

    [feat] refactor print

--- inc/response/Response.hpp
+++ inc/response/Response.hpp
@@ -19,12 +19,6 @@ public:
     ResponseImpl(const Config& config) {}
     ResponseImpl(const Request& req, const Config& conf)
     {
-		/*
-		 * IP:port
-		 * 		  /resources/path
-		 * 		  				 .ext
-		 * 							 ?k1=v1 & k2=v2
-		 */
 		std::string url = req.url;
 		std::string	host = "*:8000";	//	서버 요청 파싱 필요
 		if (url.empty())
@@ -43,47 +37,45 @@ public:
 		}
 
 		std::pair<std::string, std::string>	divpath = Util::divider(path, '/');
-		std::string	location = divpath.first;
-		std::string	resource = divpath.second;
-		//std::string	ext;
 
-		while (location != "")
+		std::string goTo;		//	Location
+		std::string ext;		//	확장자
+		std::string executer;	//	실행 바이너리
+
+		while (divpath.first != "")
 		{
-			std::cout << "[" << location << "]" <<  std::endl;
-			std::cout << "[" << resource << "]" << std::endl;
-			std::cout << std::endl;
 			try
 			{
-				std::string goTo = conf[host][location]["root"][0] + resource;
-				std::string ext(resource.begin() + resource.rfind('.'), resource.end());
-				std::string executer(conf[host][location][ext][0]);
+				/*
+				 * 최대한 디테일한 PATH를 찾고 만약 찾았다면 No Exception
+				 * 못 찾았다면 상위 PATH로 이동
+				 * /a/b/c/d.sh가 있다면
+				 * /a/b/c에서 d.sh 실행 시도
+				 * ->	/a/b에서 /c/d.sh 실행 시도
+				 * ->	->	/a에서 /b/c/d.sh 실행 시도
+				 * ->	->	->	/에서 /a/b/c/d.sh 실행 시도
+				 */
+				goTo = conf[host][divpath.first]["root"][0] + divpath.second;
+				ext = std::string(divpath.second.begin() + divpath.second.rfind('.'), divpath.second.end());
+				executer = std::string(conf[host][divpath.first][ext][0]);
+
 				std::cout << "==== Find Right Path ===" << std::endl;
 				std::cout << "Root + Resource's Path : " << goTo << std::endl;
 				std::cout << "CGI Executer : " << executer << std::endl;
 				std::cout << "=========== = ==========" << std::endl;
+				break ;
 			} catch (std::exception& e)
 			{
 				std::cerr << e.what() << std::endl;
 			}
-
 			divpath = Util::divider(divpath, '/');
-			location = divpath.first;
-			resource = divpath.second;
 		}
-		std::cout << "[" << location << "]" <<  std::endl;
-		std::cout << "[" << resource << "]" << std::endl;
-		std::cout << std::endl;
-		std::string executer;
-		/*
-		try
-		{
-			executer = conf[host][]
-		}
-		catch ()
-		{
 
-		}
-		 */
+		//	최종 Location과 root_dir이 합쳐진 root_dir + 파일 경로 존재
+		std::cout << std::endl;
+		std::cout << "location : " << divpath.first << std::endl;
+		std::cout << "resource : " << divpath.second << std::endl;
+
 		/*
         if (true)
 			mContents = new File(req.url);

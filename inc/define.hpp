9a670ca81fe439f86907fc242e5db26a4c6798d9
kangmin <kangmin5505@naver.com>
2022-11-02 18:26:19 +0900

    [feat] Add State enum

--- inc/define.hpp
+++ inc/define.hpp
@@ -6,6 +6,16 @@ enum eType {
 	CLIENT
 };
 
+enum eState {
+	NONE = 0,
+	READ_REQUEST,
+	DONE_REQUEST,
+	READ_RESPONSE,
+	WRITE_RESPONSE,
+	DONE_RESPONSE,
+	DONE
+};
+
 static const time_t		TIMEOUT = 30;
 static const int		MAX_EVENT = 1024; // TODO: I don't know optimal MAX_EVENT yet.
 const int				BUFFER_SIZE = 1024;
